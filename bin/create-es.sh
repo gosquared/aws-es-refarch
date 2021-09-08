2b0e1f9c9dca39eced265cde7a628eae4d04de0e
Geoff Wagstaff <geoff@gosquared.com>
2021-09-08 14:54:43 +0000

    set stack name

--- bin/create-es.sh
+++ bin/create-es.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-ROLE_ARN='arn:aws:iam::123456789:role/cf-role'
-TAG_KEY=role
-TAG_VALUE=aws-elasticsearch
-AWS_REGION=us-east-1
-
 aws --region $AWS_REGION cloudformation create-stack \
-  --stack-name ip-data-exporter-elasticsearch-$STAGE \
+  --stack-name "$NAME-$STAGE" \
   --template-body file://./cf-templates/es.yaml \
   --parameters ParameterKey=Stage,ParameterValue=$STAGE \
   --tags Key=$TAG_KEY,Value="$TAG_VALUE-$STAGE" \
